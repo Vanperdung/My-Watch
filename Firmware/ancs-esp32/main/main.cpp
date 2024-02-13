a078daabff213b413d3e0a7f7e5d5e25546d61e3
Vanperdung <dung.nv382001@gmail.com>
2024-02-13 18:09:42 +0700

    Firmware: delete .vscode file, add template project for ANCS

---
+++ Firmware/ancs-esp32/main/main.cpp
@@ -0,0 +1,19 @@
+// #include <stdio.h>
+#include <iostream>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include <inttypes.h>
+
+#define TAG "MAIN.CPP"
+
+extern "C" void app_main(void)
+{
+    while (1)
+    {
+        ESP_LOGI(TAG, "TEST");
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
